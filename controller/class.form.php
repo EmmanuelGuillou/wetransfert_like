97459d6a9e6f6f9884d83c70a0968e1f38fd5a18
Unknown <laurianepierre@outlook.fr>
2017-02-20 11:34:52 +0100

    fonction de création d'input

---
+++ controller/class.form.php
@@ -0,0 +1,32 @@
+<?php
+
+class FORM{
+
+    private $db;
+
+    public function __construct($pdo){
+        $this->db = $pdo;
+    }
+
+//Fonction qui crée un input email
+    public function input($name){
+        echo '<label>'.$name.'</label><input type="email" name="'.$name.'"><br/>';
+    }
+
+//Fonction qui crée un input file
+    public function input($name){
+        echo '<label>'.$name.'</label><input type="file" name="'.$name.'"><br/>';
+    }
+
+//Fonction qui crée un textarea
+    public function textarea($name){
+        echo '<label>'.$name.'</label><textarea name="'.$name.'" required></textarea><br/>';
+    }
+
+//Fonction qui crée un bouton submit
+    public function submit(){
+        echo '<button type="submit">Envoyer</button>';
+    }
+}
+
+?>
