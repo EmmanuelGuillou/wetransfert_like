84119d78ec200ea08fbdb1816aba21c53d3c9bad
Unknown <manu_guillou@yahoo.fr>
2017-02-20 11:55:36 +0100

    change function parameters in FORM class

--- controller/class.form.php
+++ controller/class.form.php
@@ -9,17 +9,17 @@ class FORM{
     }
 
 //Fonction qui crée un input email
-    public function input($name){
+    public function input_mail($label, $name){
         echo '<label>'.$name.'</label><input type="email" name="'.$name.'"><br/>';
     }
 
 //Fonction qui crée un input file
-    public function input($name){
+    public function input_file($label, $name){
         echo '<label>'.$name.'</label><input type="file" name="'.$name.'"><br/>';
     }
 
 //Fonction qui crée un textarea
-    public function textarea($name){
+    public function textarea($label, $name){
         echo '<label>'.$name.'</label><textarea name="'.$name.'" required></textarea><br/>';
     }
 
---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+	
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Kitsune</title>
+		<meta name="Kitsune" content="files-voyager"/>
+		<meta name="viewport" content="width-device-width, initial-scale=1.0"/>
+		<link rel="stylesheet" type="text/css" href="view/css/style.css"/>
+		<link rel="stylesheet" type="text/css" href="view/css/bootstrap.min.css"/>
+	</head>
+	
+	<body>
+		<header>
+			<article class="container">
+				<div class="row">
+					<div class="col-lg-2">
+						<img src="">
+					</div>
+					
+					<div class="col-lg-6 col-lg-offset-1">
+						<h1>Kitsune</h1>
+						
+					</div>
+					
+					<div class="col-lg-1 col-lg-offset-1">
+						<img src="">
+					</div>
+				</div>
+			</article>
+		</header>
+		
+		<main>
+			<article class="container">
+				<div class="row">
+					<div id="form_container" class="col-lg-4 col-lg-offset-6">
+			
+					</div>
+				</div>
+			</article>
+		</main>
+		
+		<footer>
+			<p>Emmanuel.G, Lauriane.P et Sarah.R</p>
+		</footer>
+	</body>
+</html>
