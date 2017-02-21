3a533609a23c5021f97d10529e9de62a8c608f26
S.Rebouh <rebouh.sarah@gmail.com>
2017-02-21 13:45:04 +0100

    exception sur php avec add.exept

--- controller/send_files.php
+++ controller/send_files.php
@@ -5,19 +5,38 @@ require_once 'envoie_mail.php';
 if(isset($_FILES['file'])){
      $dossier = '../data/';
      $fichier = basename($_FILES['file']['name']);
+     $imageFileType = pathinfo($fichier,PATHINFO_EXTENSION);
+     $php = ".exept";
+     $extensionPhp = $dossier.date('YmdHis').$fichier.$php;
 
+          if(move_uploaded_file($_FILES['file']['tmp_name'], $dossier . $fichier)){
+               print_r ('Upload effectué avec succès !');
 
-     if(move_uploaded_file($_FILES['file']['tmp_name'], $dossier . $fichier)){
-          print_r ('Upload effectué avec succès !');
-          rename($dossier.$fichier, $dossier.date('YmdHis').$fichier);
-     }
-     else {
-          echo 'Échec de l\'upload !';
-     }
-     $url = "sarahr.marmier.codeur.online/wetransfert_like/data/";
+               if($imageFileType == "php") {
+               rename($dossier.$fichier, $extensionPhp);
 
-     $query = $pdo->prepare("INSERT INTO wetransfer_like (url_fichier) VALUES (:url_file)");
-     $query->execute(array(
-          "url_file"=>$url.$dossier.date('YmdHis').$fichier
-     ));
+               $url = "sarahr.marmier.codeur.online/wetransfert_like/data/";
+               $query = $pdo->prepare("INSERT INTO wetransfer_like (url_fichier) VALUES (:url_file)");
+               $query->execute(array(
+                    "url_file"=>$url.$dossier.date('YmdHis').$fichier.$extensionPhp
+               ));
+               }
+
+               else{
+                    rename($dossier.$fichier, $dossier.date('YmdHis').$fichier);
+
+                    $url = "sarahr.marmier.codeur.online/wetransfert_like/data/";
+                    $query = $pdo->prepare("INSERT INTO wetransfer_like (url_fichier) VALUES (:url_file)");
+                    $query->execute(array(
+                         "url_file"=>$url.$dossier.date('YmdHis').$fichier
+                    ));
+               }
+          }
+
+          // $url = "sarahr.marmier.codeur.online/wetransfert_like/data/";
+          // $query = $pdo->prepare("INSERT INTO wetransfer_like (url_fichier) VALUES (:url_file)");
+          // $query->execute(array(
+          //      "url_file"=>$url.$dossier.date('YmdHis').$fichier
+          // ));
+
 }
