26373423c9b6b057dcd1b04af8023f2a892daf77
S.Rebouh <rebouh.sarah@gmail.com>
2017-02-21 11:06:36 +0100

    upload rename

--- controller/send_files.php
+++ controller/send_files.php
@@ -6,10 +6,18 @@ if(isset($_FILES['file'])){
      $dossier = '../data/';
      $fichier = basename($_FILES['file']['name']);
 
+
      if(move_uploaded_file($_FILES['file']['tmp_name'], $dossier . $fichier)){
           print_r ('Upload effectué avec succès !');
+          rename($dossier.$fichier, $dossier.date('YmdHis').$fichier);
      }
-     else{
+     else {
           echo 'Échec de l\'upload !';
      }
+     $url = "sarahr.marmier.codeur.online/wetransfert_like/data/";
+
+     $query = $pdo->prepare("INSERT INTO wetransfer_like (url_fichier) VALUES (:url_file)");
+     $query->execute(array(
+          "url_file"=>$url.$dossier.date('YmdHis').$fichier
+     ));
 }
