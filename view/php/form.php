01b75e6b906cacafe3af684c45e173d5669090b3
S.Rebouh <rebouh.sarah@gmail.com>
2017-02-27 09:12:15 +0100

    title form

--- view/php/form.php
+++ view/php/form.php
@@ -1,5 +1,5 @@
 	<?php $form->input_mail("E-mail d'origine", "origin_mail") ?>
-	<?php $form->input_mail("E-mail de déstination", "destination_mail") ?>
-	<?php $form->textarea("Message", "message") ?>
-	<?php $form->input_file("Mon_fichier", "file") ?>
+	<?php $form->input_mail("E-mail de destination", "destination_mail") ?>
+	<?php $form->textarea("Votre message", "message") ?>
+	<?php $form->input_file("Votre fichier", "file") ?>
 	<?php $form->submit() ?>
